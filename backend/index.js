1f6073bcb781799a7a72737f5dcc2741ad1321fe
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-09-22 23:47:13 +0530

    test_api

--- backend/index.js
+++ backend/index.js
@@ -26,10 +26,15 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     .catch((err) => console.error(err));
 
 // Routes
+
 app.use('/oauth' , oauth);
 app.use('/request' , requestRoute);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/auth', authRoutes);
+app.get('/' ,(req , res)=>{
+    
+    res.send("Hello world Backend is runing");
+} )
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
